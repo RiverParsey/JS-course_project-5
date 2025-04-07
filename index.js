f3a4ea14bcff96471ffe7c7841a78081b4fd4b21
ricsi <rihardptrk18@gmail.com>
2025-04-07 18:18:57 +0200

    lesson-4_data_sending

--- index.js
+++ index.js
@@ -1,3 +1,74 @@
+// lesson-4 FormData data sending
+
+// window.addEventListener('DOMContentLoaded', () => {
+//   const form = document.querySelector('form');
+
+//   // XMLHttpRequest
+//   function req(e) {
+//     e.preventDefault();
+
+//     let formData = new FormData(form);
+//     formData.append('id', Math.random());
+
+
+//     let obj = {};
+//     formData.forEach((value, key) => {
+//       obj[key] = value;
+//     });
+//     let json = JSON.stringify(obj);
+
+
+//     const request = new XMLHttpRequest();
+//     request.open('POST', 'url');
+//     request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+//     request.send(json);
+//     request.addEventListener('readystatechange', function() {
+//       if (request.readyState === 4 && request.status == 200) {
+//         let data = JSON.parse(request.response);
+//         console.log(data);
+//       } else {
+//         console.error('что то пошло не так')
+//       }
+//     });
+
+//     this.remove();
+//   }
+
+//   document.querySelector('button').addEventListener('click', (e) => req(e), {'once': true});
+
+//   // function createCards(data) {
+//   //   data.forEach(item => {
+//   //     let card = document.createElement('div');
+
+//   //     card.classList.add('card');
+
+//   //     let icon;
+//   //     if (item.sex === 'male') {
+//   //       icon = 'icons/mars.png';
+//   //     } else {
+//   //       icon = 'icons/female.png';
+//   //     }
+
+//   //     card.innerHTML = `
+//   //       <img src="${item.photo}" alt="">
+//   //       <div class="name">${item.name} ${item.surname}</div>
+//   //       <div class="sex">
+//   //         <img src="${icon}" alt="${item.sex}">
+//   //       </div>
+//   //       <div class="age">${item.age}</div>
+//   //     `;
+//   //     document.querySelector('.app').appendChild(card);
+//   //   });
+//   // }
+
+// });
+
+
+
+
+
+
+
 // lesson-3 fetch API and Axios
 
 // window.addEventListener('DOMContentLoaded', () => {
