f947cbd4ad99079a3dbd3bb23de47e8bdcd29bc3
ricsi <rihardptrk18@gmail.com>
2025-04-08 11:15:09 +0200

    lesson-5_create_parser

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
+// lesson-5 Parser
+
+// window.addEventListener('DOMContentLoaded', () => {
+//   const body = document.querySelector('body');
+//   let textNodes = [];
+
+//   function recursy(element) {
+//     element.childNodes.forEach(node => {
+//       if (node.nodeName.match(/^H\d/)) {
+//         const obj = {
+//           header: node.nodeName,
+//           content: node.textContent
+//         };
+//         textNodes.push(obj);
+//       } else {
+//         recursy(node);
+//       }
+//     });
+//   }
+
+//   recursy(body);
+  
+//   fetch('https://jsonplaceholder.typicode.com/posts', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(textNodes)
+//   })
+//   .then(response => response.json())
+//   .then(json => console.log(json));
+
+// });
+
+
+
+
+
+
+
 // lesson-4 FormData data sending
 
 // window.addEventListener('DOMContentLoaded', () => {
