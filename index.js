c8591429831f22d7f6ef48f35f05486f9245f639
ricsi <rihardptrk18@gmail.com>
2025-04-08 15:56:47 +0200

    lesson-7_test_task

--- index.js
+++ index.js
@@ -1,3 +1,53 @@
+// lesson-7 Test Tasks
+
+// const w = +prompt('ширина матрицы');
+// const h = +prompt('высота матрицы');
+
+// function matrix(w, h) {
+//   let result = new Array(h).fill().map(() => new Array(w).fill(''));
+//   let counter = 1;
+//   let startCol = 0;
+//   let endCol = w - 1;
+//   let startRow = 0;
+//   let endRow = h - 1;
+
+//   while (startCol <= endCol && startRow <= endRow) {
+//     for (let i = startCol; i <= endCol; i++) {
+//       result[startRow][i] = counter;
+//       counter++;
+//     }
+//     startRow++;
+
+//     for (let j = startRow; j <= endRow; j++) {
+//       result[j][endCol] = counter;
+//       counter++;
+//     }
+//     endCol--;
+
+//     for (let i = endCol; i >= startCol; i--) {
+//       result[endRow][i] = counter;
+//        counter++;
+//     }
+//     endRow--;
+
+//     for (let j = endRow; j >= startRow; j--) {
+//       result[j][startCol] = counter;
+//       counter++;
+//     }
+//     startCol++;
+//   }
+
+//   return result;
+// }
+
+// console.log(matrix(w, h));
+
+
+
+
+
+
+
 // lesson-6 Test Tasks
 
 // const roles = [`Городничий`, `Аммос Федорович`, `Артемий Филиппович`, `Лука Лукич`, `Христиан Иванович`]
